0471d2ba200d41bbc69495edb7fea1a6ad98e5f6
Folke Will <folko@solhost.org>
2019-10-02 20:17:16 +0200

    Separate client and server app

--- src/node/lib/socdp8.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UIOMapper_1 = require("./UIO/UIOMapper");
-let mapper = new UIOMapper_1.UIOMapper();
-let buffer = mapper.mapUio('socdp8_core', 'socdp8_core_mem');
-console.log(buffer.readUInt16LE(0));
